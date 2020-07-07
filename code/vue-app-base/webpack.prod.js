4e3e30b5982ccc6e51498c23a393381ca7236395
彭淦 <penggan@penggans-MacBook-Air.local>
2020-07-07 09:06:01 +0800

    .

--- code/vue-app-base/webpack.prod.js
+++ code/vue-app-base/webpack.prod.js
@@ -21,9 +21,9 @@ module.exports = merge(common, {
      * still all optimizations are available for manual configuration and overrides.
      */
     optimization: {
-        usedExports: true,
-        minimize: true,
-        concatenateModules: true,
+        usedExports: true,      //打包过程移除未被引用的模块
+        minimize: true,             //压缩js代码；进行 tree shaking; production 模式默认开启该配置
+        concatenateModules: true,   //根据模块依赖关系图谱，把模块打包到单个模块文件中；production 模式默认开启该配置
         sideEffects: true,
     }
 })
